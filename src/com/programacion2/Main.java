cf8137b64e992f2fdce94361bd1eddd27c469993
Juan Vazquez Veloso <jvazquezveloso@danielcastelao.org>
2021-01-12 10:59:42 +0100

    nueva rama bugFix

--- src/com/programacion2/Main.java
+++ src/com/programacion2/Main.java
@@ -3,6 +3,7 @@ package com.programacion2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Rama principal");
+        System.out.println("Salgo de la rama principal");
+        System.out.println("Voy a la bugFix");
     }
 }
