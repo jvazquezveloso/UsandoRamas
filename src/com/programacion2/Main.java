791a497ffcada9642a9a5731ef392e0709e9ea02
Juan Vazquez Veloso <jvazquezveloso@gmail.com>
2021-03-24 21:26:15 +0100

    merge

--- src/com/programacion2/Main.java
+++ src/com/programacion2/Main.java
@@ -3,6 +3,8 @@ package com.programacion2;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Rama principal");
+        System.out.println("Merge");
     }
 }
