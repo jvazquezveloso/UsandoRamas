441d598b50846387ad55c7532080739459c1dd79
Juan Vazquez Veloso <jvazquezveloso@gmail.com>
2021-03-24 21:25:01 +0100

    commit

--- src/com/programacion2/Main.java
+++ src/com/programacion2/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Salgo de la rama principal");
         System.out.println("Voy a la bugFix");
+        System.out.println("Otro commit");
     }
 }
